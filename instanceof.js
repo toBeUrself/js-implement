ba68b8ecb60f8b98c4fcbe684fff99d1c6908c90
Tim <liuzeyun@163.com>
2019-09-08 10:35:54 +0800

    add my object.create js

--- instanceof.js
+++ instanceof.js
@@ -1,9 +1,9 @@
 // 思路：右边变量的原型存在于左边变量的原型链上
-function MyInstanceOf (leftVal, rightVal) {
+function MyInstanceOf(leftVal, rightVal) {
     let leftProto = leftVal.__proto__;
     let rightProto = rightVal.prototype;
 
-    while(true) {
+    while (true) {
         if (!leftProto) {
             return false;
         } else if (leftProto === rightProto) {
